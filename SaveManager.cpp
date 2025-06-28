6bd88f1f6e3a1299d12d3f6bfbd94590da523567
jonbassoon25 <124705409+jonbassoon25@users.noreply.github.com>
2025-06-28 09:50:34 -0700

    Add thread process

--- SaveManager.cpp
+++ SaveManager.cpp
@@ -1,11 +1,27 @@
+// Main script for SkyrimSaveManager
+
+#include <thread>
+#include <chrono>
+
+void LogDebugMsg(const std::string message) {
+    RE::ConsoleLog::GetSingleton()->Print(message.c_str());
+}
+
+void TestRunner() {
+    LogDebugMsg("Hello from a thread!");
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    LogDebugMsg("Hello after 10 seconds!");
+}
+
 SKSEPluginLoad(const SKSE::LoadInterface *skse) {
     SKSE::Init(skse);
 
-    // Once all plugins and mods are loaded, then the ~ console is ready and can
-    // be printed to
-    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message *message) {
-        if (message->type == SKSE::MessagingInterface::kDataLoaded)
-            RE::ConsoleLog::GetSingleton()->Print("Hello, world!");
+    // Start subprocess for save management after other mods are loaded
+    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* message) {
+        if (message->type == SKSE::MessagingInterface::kDataLoaded) {
+            std::thread task(TestRunner);
+            task.detach();
+        }
     });
 
     return true;
