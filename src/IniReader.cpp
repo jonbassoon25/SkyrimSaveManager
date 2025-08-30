fa5437de6764bad777eb5fb3fc6fda6c733c9871
jonbassoon25 <124705409+jonbassoon25@users.noreply.github.com>
2025-08-30 15:12:54 -0700

    Add IniReader comments

--- src/IniReader.cpp
+++ src/IniReader.cpp
@@ -6,11 +6,19 @@ IniReader::IniReader(const std::string& path, const std::string& iniSection)
 	this->iniSection = iniSection;
 }
 
+/*
+Reads the integer value of key from the file and section of this reader. 
+If no value is found, the default is returned.
+*/
 int IniReader::ReadInt(const std::string& key, int default_)
 {
 	return static_cast<int>(GetPrivateProfileIntA(iniSection.c_str(), key.c_str(), default_, iniPath.c_str()));
 }
 
+/*
+Reads the boolean value of key from the file and section of this reader.
+If no value is found, the default is returned.
+*/
 bool IniReader::ReadBool(const std::string& key, bool default_)
 {
 	char buffer[8] = {};
@@ -20,6 +28,10 @@ bool IniReader::ReadBool(const std::string& key, bool default_)
 	return value_str == "1" || value_str == "true" || value_str == "True" || value_str == "TRUE";
 }
 
+/*
+Reads the double value of key from the file and section of this reader.
+If no value is found, the default is returned.
+*/
 double IniReader::ReadDouble(const std::string& key, double default_)
 {
 	char buffer[32] = {};
@@ -33,6 +45,11 @@ double IniReader::ReadDouble(const std::string& key, double default_)
 	}
 }
 
+/*
+Reads the string value of key from the file and section of this reader.
+If no value is found, the default is returned.
+The maximum string length that can be read is 254
+*/
 std::string IniReader::ReadString(const std::string& key, const std::string& default_)
 {
 	char buffer[255] = {};
