998fdb23b9f2a54706dfc06d235837dd1d8a347f
jonbassoon25 <124705409+jonbassoon25@users.noreply.github.com>
2025-08-31 14:43:45 -0700

    Add MainManager headers

--- include/MainManager.h
+++ include/MainManager.h
@@ -1,1 +1,22 @@
 #pragma once
+
+class MainManager
+{
+private:
+	UserVars userVariables;
+	std::unordered_map<UINT32, GameManager> GamesById;
+
+	std::string CalcIniPath() const;
+	std::string CalcDocumentsPath() const;
+	std::string CalcLocalSavePath() const;
+	std::string CalcSavePath() const;
+
+
+public:
+	MainManager();
+
+	void Refresh();
+	void Reset();
+
+	float GetPollTime();
+};
